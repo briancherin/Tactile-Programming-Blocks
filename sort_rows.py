2db995a1869f9bd75240a954dcae5400d6e5fce5
Brian Cherin <brian.cherin@gmail.com>
2018-12-17 13:02:56 -0500

    Fix function for printing out block row list

--- sort_rows.py
+++ sort_rows.py
@@ -9,19 +9,39 @@ import block_helper
 import cv2
 import imutils
 
+
+RED = block_helper.RED
+YELLOW = block_helper.YELLOW
+GREEN = block_helper.GREEN
+AQUA = block_helper.AQUA
+BLUE = block_helper.BLUE
+PURPLE = block_helper.PURPLE
+
+#function chart
+IF = {BLUE, GREEN} #if - red-yellow
+TRUE = {RED, GREEN} #true - red-green
+FALSE = {RED, AQUA} #false - red-aqua
+WHILE = {RED, BLUE} #while - red-blue
+SAY = {RED, PURPLE} #say - red-purple
+LED = {YELLOW, RED} #LED - yellow-green
+
+blockIdentities = {
+	tuple(IF): "IF",
+	tuple(TRUE): "TRUE",
+	tuple(FALSE): "FALSE",
+	tuple(WHILE): "WHILE",
+	tuple(SAY): "SAY",
+	tuple(LED): "LED"
+}
+
+
 def takeFirst(elem):
     return elem[0]
     
-
-img = cv2.imread("three_blocks.jpg")
-img = imutils.resize(img, width = 400)
-img_color, contourList = block_helper.getContoursFromImage(img) #contours are given in order of decreasing area
-
-coordinate_list = []
-height_list = []
-
-
 def sortIntoRows(contourList):
+
+	coordinate_list = []
+
 	# add the contours to a list
 	for contour in contourList:
 		x, y, width, height = cv2.boundingRect(contour)
@@ -50,4 +70,27 @@ def sortIntoRows(contourList):
 				count += 1
 			# end steps 3 & 4
 	# end step 5
+	return row_list
 
+#Add to the list the indent_space (distance from left edge of image) for the first block of each row.
+def markIndentSpace(rowList):
+	for x in range (0, len(rowList)):
+		row_indent_space = rowList[x][0][0] #The horizontal dispacement of the first block in the row (amount of space indented)
+		rowList[x] = [row_indent_space, rowList[x]] #Each row becomes a list containing the indent_space and the list of block contour info
+	return rowList
+
+def printBlockRowList(blockRowList):
+	#(rowList with indents)
+	print blockRowList
+	for row in blockRowList:
+		print "indent " + str(row[0]) + ", " + "".join(blockIdentities[tuple(block)] for block in row[1])
+		#print "indent " + str(row[0]) + ", " + "".join(block for block in row[1])
+	
+
+img = cv2.imread("./test_images/paper_blocks2.jpg")
+img = imutils.resize(img, width = 400)
+img_color, contourList = block_helper.getContoursFromImage(img) #contours are given in order of decreasing area
+rowList = sortIntoRows(contourList)
+rowListWithIndentSpace = markIndentSpace(rowList)
+blockRowList = block_helper.getBlockListFromRowList(img_color, rowListWithIndentSpace);
+printBlockRowList(blockRowList)
